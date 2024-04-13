616aede46207672b10398e52282d296415f2f298
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-13 18:37:08 -0500

    create routing system for multiple pages

--- hack-ku-2024/src/App.tsx
+++ hack-ku-2024/src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss'
-import HomePage from './HomePage'
+import HomePage from './Pages/HomePage'
+import CameraPage from './Pages/CameraPage';
+import NotFoundPage from './Pages/NotFoundPage';
+import NavigationButtons from './NavigationButtons/NavigationButtons';
 
 function App() {
   return (
-    <>
-      <HomePage></HomePage>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage/>}/>
+        <Route path="camera" element={<CameraPage/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
+      <NavigationButtons/>
+    </BrowserRouter>
   );
 }
 
