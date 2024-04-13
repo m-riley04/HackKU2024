1def96d22ae52bfb6cf8b21a9b3a557b790cddee
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-12 23:51:50 -0500

    add handleSetUser function

--- hack-ku-2024/src/App.tsx
+++ hack-ku-2024/src/App.tsx
@@ -13,6 +13,14 @@ interface User {
 function App() {
   const [user, setUser] = useState<User | undefined>(undefined);
 
+  async function handleSetUser(id:number) {
+    await fetch(`http://localhost:8080/api/users/${id}`)
+    .then(res => res.json())
+    .then(data => { setUser(data.data); console.log(user) }) // Set the user state
+    .catch(e => {
+      console.error("Failed to fetch user:", e);
+    });
+  }
 
   return (
     <>
