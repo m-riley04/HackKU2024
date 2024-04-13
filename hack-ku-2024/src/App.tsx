5edfaf270f81c1bc80781ddfa75c203a25ada139
soulsoiledit <soulsoill@proton.me>
2024-04-13 00:36:28 -0500

    Add basic map and marker components

--- hack-ku-2024/src/App.tsx
+++ hack-ku-2024/src/App.tsx
@@ -1,25 +1,29 @@
-import './App.css'
-import { useEffect, useState } from "react"
-import UserStats from './UserStats/UserStats'
+import "./App.css";
+import { useEffect, useState } from "react";
+import UserStats from "./UserStats/UserStats";
+import QuickMap from "./Map/QuickMap";
 
 interface User {
-  id: number
-  name: string
-  xp: number
-  nextLevelXP: number
-  level: number
+  id: number;
+  name: string;
+  xp: number;
+  nextLevelXP: number;
+  level: number;
 }
 
 function App() {
   const [user, setUser] = useState<User | undefined>(undefined);
 
-  async function handleSetUser(id:number) {
+  async function handleSetUser(id: number) {
     await fetch(`http://localhost:8080/api/users/${id}`)
-    .then(res => res.json())
-    .then(data => { setUser(data.data); console.log(user) }) // Set the user state
-    .catch(e => {
-      console.error("Failed to fetch user:", e);
-    });
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.data);
+        console.log(user);
+      }) // Set the user state
+      .catch((e) => {
+        console.error("Failed to fetch user:", e);
+      });
   }
 
   useEffect(() => {
@@ -28,12 +32,16 @@ function App() {
       const currentUserId = 3;
 
       // Get the current user from the database
-      handleSetUser(currentUserId)
+      handleSetUser(currentUserId);
     }
   }, [user]);
 
   if (!user) {
-    return <><p>Loading user...</p></>
+    return (
+      <>
+        <p>Loading user...</p>
+      </>
+    );
   }
 
   return (
@@ -41,10 +49,11 @@ function App() {
       <UserStats user={user}></UserStats>
       <div>
         <h1>{user.name}</h1>
+        <QuickMap />
         <button>Click here to play</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
