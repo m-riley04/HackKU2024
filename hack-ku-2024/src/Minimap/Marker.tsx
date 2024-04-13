d2f853467b6c52a68e0cb6b6f886cdfaec35bbd3
soulsoiledit <soulsoill@proton.me>
2024-04-13 10:49:07 -0500

    move Map into MiniMap

--- hack-ku-2024/src/Minimap/Marker.tsx
+++ hack-ku-2024/src/Minimap/Marker.tsx
@@ -6,25 +6,27 @@ import {
   useAdvancedMarkerRef,
 } from "@vis.gl/react-google-maps";
 
-function CustomMarker({ latitude, longitude } : { latitude: number, longitude: number}) {
+function CustomMarker({ name, id, latitude, longitude, image }) {
   const [infowindowOpen, setInfowindowOpen] = useState(true);
   const [markerRef, marker] = useAdvancedMarkerRef();
 
   return (
     <>
       <AdvancedMarker
+        className={name}
         ref={markerRef}
         onClick={() => setInfowindowOpen(true)}
         position={{ lat: latitude, lng: longitude }}
         title={"Stub for marker"}
       />
+
       {infowindowOpen && (
         <InfoWindow
           anchor={marker}
           maxWidth={200}
           onCloseClick={() => setInfowindowOpen(false)}
         >
-          <img height="100" src="./images/leep.jpg" />
+          <img height="100" src={image} />
         </InfoWindow>
       )}
     </>
--- hack-ku-2024/src/Minimap/Minimap.tsx
+++ hack-ku-2024/src/Minimap/Minimap.tsx
@@ -1,11 +1,35 @@
 import "./Minimap.scss"
-function Minimap() {
 
-    return (
-      <div className="map">
-        
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useGeolocated } from "react-geolocated";
+
+import CustomMarker from "./Marker.tsx";
+
+function Minimap() {
+    const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+      useGeolocated();
+    const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+    return !(isGeolocationEnabled && isGeolocationAvailable && coords) ? (
+      <div className="map_error">
+        <p> Failed to get geolocation data </p>
       </div>
-    )
-  }
-  
-  export default Minimap
+      ): (
+      <div className="map">
+      <APIProvider apiKey={API_KEY}>
+        <Map
+          mapId={"somethingunique"}
+          defaultCenter={{ lat: coords.latitude, lng: coords.longitude }}
+          defaultZoom={15}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+        >
+          <CustomMarker name="test" id="test" latitude={coords.latitude} longitude={coords.longitude} image="./images/leep.jpg" />
+
+        </Map>
+      </APIProvider>
+      </div>
+    );
+}
+
+export default Minimap
