cd82475356dcb94ee83695b4f34399331984679c
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-13 13:31:29 -0500

    set to app url

--- hack-ku-2024/src/HomePage.tsx
+++ hack-ku-2024/src/HomePage.tsx
@@ -5,6 +5,7 @@ import { Button } from 'react-bootstrap'
 import Minimap from './Minimap/Minimap'
 import { User } from "./interfaces"
 import Camera from 'react-html5-camera-photo'
+const APP_URL = "https://hackku2024-lz3sc7ogqa-uc.a.run.app"
 
 function HomePage() {
   const [user, setUser] = useState<User | undefined>(undefined);
@@ -15,7 +16,7 @@ function HomePage() {
    * @param id a user Id
    */
   async function handleSetUser(id:number) {
-    await fetch(`http://localhost:8080/api/users/${id}`)
+    await fetch(`${APP_URL}/api/users/${id}`)
     .then(res => res.json())
     .then(data => { setUser(data.data); console.log(user) }) // Set the user state
     .catch(e => {
