4bffc2cd06652b523a86067d1ccf4d30a4f14d5f
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-13 15:09:31 -0500

    fix misspelling

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function getUser(id) {
         {id: 1, name: "Riley", xp: 10, nextLevelXP: 20, level: 2},
         {id: 2, name: "Ryland", xp: 5, nextLevelXP: 10, level: 1},
         {id: 3, name: "Averi", xp: 2, nextLevelXP: 300, level: 5},
-        {id: 4, name: "Ayren", xp: 0, nextLevelXP: 50, level: 3},
+        {id: 4, name: "Aryen", xp: 0, nextLevelXP: 50, level: 3},
     ];
     return users.find(p => p.id == id);
 }
