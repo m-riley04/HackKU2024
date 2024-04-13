b299f6e11348424b39c07a9431874a95d3648721
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-13 18:59:19 -0500

    add splash page and other pages

--- hack-ku-2024/src/App.tsx
+++ hack-ku-2024/src/App.tsx
@@ -4,13 +4,19 @@ import HomePage from './Pages/HomePage'
 import CameraPage from './Pages/CameraPage';
 import NotFoundPage from './Pages/NotFoundPage';
 import NavigationButtons from './NavigationButtons/NavigationButtons';
+import SplashPage from './Pages/SplashPage';
+import LeaderboardPage from './Pages/LeaderboardPage';
 
 function App() {
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<HomePage/>}/>
+        <Route index element={<SplashPage/>}/>
+        <Route path="map" element={<HomePage/>}/>
         <Route path="camera" element={<CameraPage/>}/>
+        <Route path="leaderboard" element={<LeaderboardPage/>}/>
         <Route path="*" element={<NotFoundPage/>}/>
       </Routes>
       <NavigationButtons/>
