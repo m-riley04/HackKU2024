d62a0c0977720773c3ec62e94bdfc985dae50177
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-13 13:06:10 -0500

    add cors checking

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static("hack-ku-2024/dist"));
 const port = process.env.PORT || 8080;
