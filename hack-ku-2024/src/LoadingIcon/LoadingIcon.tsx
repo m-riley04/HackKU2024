3fc1574b6a16d968ef57afd4c6d397b6554f68c3
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-14 06:04:53 -0500

    Update LoadingIcon.tsx

--- hack-ku-2024/src/LoadingIcon/LoadingIcon.tsx
+++ hack-ku-2024/src/LoadingIcon/LoadingIcon.tsx
@@ -4,7 +4,7 @@ import loadingGif from "../../images/loading.gif"
 function LoadingIcon() {
 
     return (
-        <img width="100px" height="100px" src={loadingGif}></img>
+        <img width="100px" height="100px" style={{width: "100px", height: "100px", margin: "auto"}} src={loadingGif}></img>
     );
 }
 
