c79fe1680de4c23f053ca02c8dddaca4743f5587
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-13 19:00:00 -0500

    Update HomePage.tsx

--- hack-ku-2024/src/Pages/HomePage.tsx
+++ hack-ku-2024/src/Pages/HomePage.tsx
@@ -8,14 +8,6 @@ import "react-html5-camera-photo/build/css/index.css";
 
 const APP_URL = "https://hackku2024-lz3sc7ogqa-uc.a.run.app";
 
-/**
- * Processes an image using GPT Vision to determine the material that it is made out of. The possible materials are paper, plastic, metal, glass, styrofoam, and other.
- * @param uri a public image uri
- * @returns {string} a string that is a single-word defining the material
- * 
- * @throws {Error} if the API/GPT returns nothing, or if it returns "Error" as a string
- */
-
 function HomePage() {
   const [user, setUser] = useState<User | undefined>(undefined);
 
